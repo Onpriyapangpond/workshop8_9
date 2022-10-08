2349d5c0b32163e774c71ce303f49dc01d2cbe62
Onpriyapangpond <92087339+Onpriyapangpond@users.noreply.github.com>
2022-10-08 17:06:40 +0700

    Add files via upload

---
+++ workshop8_9.php
@@ -0,0 +1,16 @@
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+
+<form action="sendredict.php" method="post">
+username: <input type="text" name="username"><br>
+password: <input type="text" name="password"><br>
+name: <input type="text" name="name"><br>
+address: <textarea name="address" rows="4" cols="40"></textarea><br>
+mobile <input type="tel" name="mobile"><br>
+email: <input type="email" name="email"><br>
+<input type="submit" value="เพิ่ม ">
+</form>
+</body>
+</html>
+
